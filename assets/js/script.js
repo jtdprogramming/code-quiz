d74558cb6fbe58e22ebd40771df2894add4741dd
James D <jtd.programming@gmail.com>
2021-11-28 18:44:09 -0500

    start-btn function and hide test

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@
 - user is displayed a question with 4 multiple choice answers
   > if correct answer chosen briefly display "correct" in footer message container  
   > if incorrect answer chosen briefly display "wrong" in the foot message container 
-
+=======================================================
+Heavily comment use of javascript for personal studies
+=======================================================
 /* 
 Question container functionality:
 Title: question goes here and changes wit
@@ -16,3 +18,23 @@ Title: question goes here and changes wit
 //   event.preventDefault();
 //   var taskNameInput = document.querySelector("input[name='task-name'").value;
 //   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+let questionContainerElement = document.getElementById('question-container')    //can use var or const depending on data type
+let startButton = document.getElementById('start-btn')
+
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+  // console.log('Button Test')    
+  startButton.classList.add('hide')    //classList.add and classList.remove to manipulate any elements **Utility 
+
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
