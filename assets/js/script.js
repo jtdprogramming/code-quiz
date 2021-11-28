1ade36b7481bdba7b7c312bffb694304d16a5b57
James D <jtd.programming@gmail.com>
2021-11-27 21:15:05 -0500

    html and css updated, flex question container

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@
 - On "start quiz" click, a timer begins counting down
   > if it reaches 0 the game ends; if the user answers all the questions, the game ends
 - user is displayed a question with 4 multiple choice answers
-  > if correct answer chosen briefly display "correct" in footer message container  **start with alert and refactor to footer container
-  > if incorrect answer chosen briefly display "wrong" in the foot message container **start with alert and refactor to footer container
+  > if correct answer chosen briefly display "correct" in footer message container  
+  > if incorrect answer chosen briefly display "wrong" in the foot message container 
 
 /* 
 Question container functionality:
